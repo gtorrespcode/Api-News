1a9e1c5b58f0896a3bcfff81ab35c85a099c08eb
Gabriel Torres <gabrieltorresp2009@gmail.com>
2023-07-05 16:55:28 -0300

    Create, findAll and FindById added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,13 @@
 const userService = require("../services/user.service");
+const mongoose = require("mongoose");
+
 const create = async (req, res) => {
   const { name, username, email, password, avatar, background } = req.body;
   if (!name || !username || !email || !password || !avatar || !background) {
     res.status(400).send({ message: "Missing information" });
   }
 
-  const user = await userService.create(req.body);
+  const user = await userService.createService(req.body);
 
   if (!user) {
     return res.status(400).send({ message: "Error while creating User" });
@@ -17,4 +19,31 @@ const create = async (req, res) => {
   });
 };
 
-module.exports = { create };
+const findAll = async (req, res) => {
+  const users = await userService.findAllService();
+
+  if (users.length === 0 ){
+    return res.status(400).send({message: "There are no users"})
+  }
+
+  res.send(users);
+
+}
+
+const findById = async (req, res) => {
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(400).send({message: "Object Id isn't valid"})
+  }
+
+  const user = await userService.findByIdService(id);
+
+  if (!user){
+    return res.status(400).send({message: "This user doesn't exist"})
+  }
+
+  res.send(user);
+}
+
+module.exports = { create, findAll, findById };
