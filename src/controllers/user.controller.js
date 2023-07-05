93ab674641ff79f2b6cf392f36aa9ae6fcb75e52
Gabriel Torres <gabrieltorresp2009@gmail.com>
2023-07-05 19:22:37 -0300

    Update Service

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,35 +15,65 @@ const create = async (req, res) => {
 
   res.status(200).send({
     message: "User created successfully",
-    user: { id: user._id, name, username, email, password, avatar, background }
+    user: { id: user._id, name, username, email, password, avatar, background },
   });
 };
 
 const findAll = async (req, res) => {
   const users = await userService.findAllService();
 
-  if (users.length === 0 ){
-    return res.status(400).send({message: "There are no users"})
+  if (users.length === 0) {
+    return res.status(400).send({ message: "There are no users" });
   }
 
   res.send(users);
-
-}
+};
 
 const findById = async (req, res) => {
   const id = req.params.id;
 
-  if (!mongoose.Types.ObjectId.isValid(id)){
-    return res.status(400).send({message: "Object Id isn't valid"})
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ message: "Object Id isn't valid" });
   }
 
   const user = await userService.findByIdService(id);
 
-  if (!user){
-    return res.status(400).send({message: "This user doesn't exist"})
+  if (!user) {
+    return res.status(400).send({ message: "This user doesn't exist" });
   }
 
   res.send(user);
-}
+};
 
-module.exports = { create, findAll, findById };
+const update = async (req, res) => {
+  const { name, username, email, password, avatar, background } = req.body;
+  if (!name && !username && !email && !password && !avatar && !background) {
+    res.status(400).send({ message: "Submit at least one field" });
+  }
+
+  const id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ message: "Object Id isn't valid" });
+  }
+
+  const user = await userService.findByIdService(id);
+
+  if (!user) {
+    return res.status(400).send({ message: "This user doesn't exist" });
+  }
+
+  await userService.updateService(
+    id,
+    name,
+    username,
+    email,
+    password,
+    avatar,
+    background
+  );
+
+  res.send({ message: "User updated!" });
+};
+
+module.exports = { create, findAll, findById, update };
